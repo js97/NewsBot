b60227f30b36a2886d2ba5bd062f86f7a74e80b6
Joshua Steubs <joshua.steubs@gmail.com>
2017-05-12 02:08:57 -0700

    Fixed subreddit frequency checks

--- NewsBot.py
+++ NewsBot.py
@@ -1,33 +1,36 @@
 import praw
 import time
 import sched
-
+import datetime
 
 def getSubmissions():
 
 	reddit_instance = praw.Reddit('bot1')
 
 	subs = []
-	subs.append(reddit_instance.subreddit('news/top/?sort=top&t=week'))
-	subs.append(reddit_instance.subreddit('economics/top/?sort=top&t=week'))
-	subs.append(reddit_instance.subreddit('chomsky/top/?sort=top&t=week'))
-	subs.append(reddit_instance.subreddit('economics/top/?sort=top&t=week'))
-	subs.append(reddit_instance.subreddit('programming/top/?sort=top&t=week'))
+	subs.append(reddit_instance.subreddit('news'))
+	subs.append(reddit_instance.subreddit('economics'))
+	subs.append(reddit_instance.subreddit('chomsky'))
+	subs.append(reddit_instance.subreddit('programming'))
+	subs.append(reddit_instance.subreddit('rust'))
+	subs.append(reddit_instance.subreddit('python'))
+	subs.append(reddit_instance.subreddit('machinelearning'))
 
 	size = len(subs)
-
+	count = 0
 	for x in range(0,size):
 		sub = subs.pop()
-		for submission in sub.hot(limit=5):
-			print('bot adding :', submission.title)
+		for submission in sub.top('week',limit=2):
+			print('\nADD: ', submission.title)
 			
 def main():
 	
 	DayOfWeek = datetime.datetime.today().weekday()
-	
-	# Is Sun. night or Mon. morning; we post for monday
-	if DayOfWeek == 6
-		getSubmissions()
+	print('Waiting...\n')
+	time.sleep(1)
+	#if DayOfWeek == 6:
+	getSubmissions()
 	
 if __name__ == '__main__':
-	main()
+	while 1:
+		main()
