42dc36f0b723174dcc123aaa29ce87e3b724ec98
Joshua Steubs <joshua.steubs@gmail.com>
2017-05-14 01:48:28 -0700

    changes

--- NewsBot.py
+++ NewsBot.py
@@ -2,6 +2,9 @@ import praw
 import time
 import sched
 import datetime
+import smtplib
+from email.mime.text import MIMEText
+
 
 def getSubmissions():
 
@@ -15,21 +18,43 @@ def getSubmissions():
 	subs.append(reddit_instance.subreddit('rust'))
 	subs.append(reddit_instance.subreddit('python'))
 	subs.append(reddit_instance.subreddit('machinelearning'))
+	subs.append(reddit_instance.subreddit('cpp'))
 
-	size = len(subs)
-	count = 0
-	for x in range(0,size):
-		sub = subs.pop()
-		for submission in sub.top('week',limit=2):
-			print('\nADD: ', submission.title)
-			
+	#size = len(subs)
+	#count = 0
+	#for x in range(0,size):
+	#	sub = subs.pop()
+	#	for submission in sub.top('week',limit=2):
+	#		print('\nADD: ', submission.title)
+	return subs
+	
+def prompt(prompt):
+    return input(prompt).strip()
+
+def sendEmail(msg, sub_list):
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.starttls()
+	email = ''
+	password = ''
+	server.login(email,password)
+	server.set_debuglevel(1)
+	server.sendmail(email,email, msg)
+	server.quit()	
+
 def main():
 	
 	DayOfWeek = datetime.datetime.today().weekday()
 	print('Waiting...\n')
 	time.sleep(1)
-	#if DayOfWeek == 6: #comment out for now, uncomment when working build is ready
-	getSubmissions()
+	#if DayOfWeek == 6:
+	with open('file.txt') as fp:
+		msg = MIMEText(fp.read())
+	email = ''
+	msg['Subject'] = 'The contents'
+	msg['From'] = 'email'
+	msg['To'] = 'email'
+
+	sendEmail(msg, getSubmissions())
 	
 if __name__ == '__main__':
 	while 1:
