54319f5ac5dc060b4bf9589343fe9c7a1c6d0f06
Joshua Steubs <joshua.steubs@gmail.com>
2017-06-21 22:04:52 -0700

    blog added

--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
-
 def index(request):
 		return render(request, 'personal/home.html')
 # Create your views here.
+
+def contact(request):
+	return render(request, 'personal/basic.html', {'content':['If you would like to contact me bladhg afd asdf a','adfawef@gmail.com']})
